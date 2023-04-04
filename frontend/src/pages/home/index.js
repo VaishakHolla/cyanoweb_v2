ac9c49dbc27b26babd478542fdbfc50d31b41e61
Vaishak Holla <vaishakholla7@gmail.com>
2023-04-04 13:40:04 -0400

    updated css for home and navbar

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import background_img from "../../assets/Home_Background.jpeg";
 const styles = {
-    header: {
-      backgroundImage: `url(${background_img})`,
-      height: '100vh',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover'
-    },
-  
-    content: {
-      height: '100%',
-      width: '100%',
-      fontSize:'2.5em',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      color: 'white'
-    }
-  }
+  header: {
+    backgroundImage: `url(${background_img})`,
+    height: "100vh",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+
+  content: {
+    height: "100%",
+    width: "100%",
+    fontSize: "2.5em",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    color: "white",
+  },
+};
 const Home = () => {
   const homeText =
     "Cyano WEB is a web infrastructure to help store and analyze water data related to CyanoHABs virtually.";
@@ -26,23 +26,13 @@ const Home = () => {
         backgorundColor: "rgba(0,0,0,0.4)",
       }}
     >
-      <div
-        style={
-          
-        //   backgroundImage: `url(${background_img})`,
-        //   backgroundPosition: "center",
-        //   backgroundPosition: "absolute",
-        //   overflow: "x-hidden",
-        //   backgroundRepeat: "no-repeat",
-        //   backgroundSize: "cover",
-        //   height: "100vh",
-        styles.header
-        }
-      >
-        
-          <div style={styles.content}>
-            {homeText}
-        
+      <div style={styles.header}>
+        <div style={styles.content}>
+          <div className="grid">
+            <div className="col-6 " style={{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh"}}>
+              {homeText}
+            </div>
+          </div>
         </div>
       </div>
     </div>
